13ca4414e2031a1a847820c0bb9f0b7926af590d
mayuozk <160881718+mayuozk@users.noreply.github.com>
2024-10-03 09:04:05 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+//Drama,romance,violencia,fantasia,aventura,ação
+//supernatural,16,drama,fantasia 
+//euphoria,18,drama,violencia 
+//You,18,thriller
+//The last of us,18,violencia,drama
+//Baby Reineer,18,violencia
+//Outer banks,16,Ação,aventura,drama
+//The summer i turnrd pretty,16,drama,romance
+//The Winchester,16,drama,fantasia
+function setup() {
+  createCanvas(400, 400);
+
+  let campoIdade
+
+function draw() {
+  background(220);
+}
