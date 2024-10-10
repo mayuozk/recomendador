f4801a66fc284769922766b6cd1b032a73370196
mayuozk <160881718+mayuozk@users.noreply.github.com>
2024-10-10 09:08:03 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,45 +4,6 @@ let campoAventura;
 
 function setup() {
   createCanvas(800, 400);
-  createElement("h2", "Recomendador de séries");
-  createSpan("Sua idade:");
-  campoIdade = createInput("5");
-  campoFantasia = createCheckbox("Gosta de fantasia?");
-  campoAventura = createCheckbox("Gosta de aventura?");
-}
-
-function draw() {
-  background("white");
-  let idade = campoIdade.value();
-  let gostaDeFantasia = campoFantasia.checked();
-  let gostaDeAventura = campoAventura.checked();
-  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
-
-  fill(color(76, 0, 115));
-  textAlign(CENTER, CENTER);
-  textSize(38);
-  text(recomendacao, width / 2, height / 2);
-}
-
-function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
-  if (idade >= 10) {
-    if (idade >= 14) {
-      return "supernatural";
-    } else {
-      if (idade >= 12) {
-        if(gostaDeFantasia || gostaDeAventura) {
-          return "the winchetrs";          
-        } else{
-         return "Euphoria";
-        }
-      } else {
-        if (gostaDeFantasia) {
-          return "let campoIdade;
-let campoFantasia;
-let campoAventura;
-
-function setup() {
-  createCanvas(800, 400);
   createElement("h2", "Recomendador de filmes");
   createSpan("Sua idade:");
   campoIdade = createInput("5");
@@ -66,41 +27,27 @@ function draw() {
 function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
   if (idade >= 10) {
     if (idade >= 14) {
-      return "you";
+      return "O menino que descobriu o vento";
     } else {
       if (idade >= 12) {
         if(gostaDeFantasia || gostaDeAventura) {
-          return "the winchesters";          
+          return "Homem aranha: no aranhaverso";          
         } else{
-         return "Euphoria";
+         return "Ladrões de bicicleta";
         }
       } else {
         if (gostaDeFantasia) {
-          return "supernatrural";
+          return "As aventuras de pi";
         } else {
-          return "13 reaons why";
+          return "Depois da chuva";
         }
       }
     }
   } else {
     if (gostaDeFantasia) {
-      return "the idol";
+      return "A viagem de chihiro";
     } else {
-      return the vampire diaries";
-    }
-  }
-}
-";
-        } else {
-          return "The summer i turned pretty";
-        }
-      }
-    }
-  } else {
-    if (gostaDeFantasia) {
-      return "outer banks";
-    } else {
-      return "Gosipy girl";
+      return "O feitiço do tempo";
     }
   }
 }
